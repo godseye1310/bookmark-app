a05749977d09a953f30eb65a741803c4c35a0793
Avinash <avinashkmg1310@gmail.com>
2024-08-21 09:08:50 +0530

    save theme locally and some styling

--- src/store/theme-context.js
+++ src/store/theme-context.js
@@ -4,15 +4,18 @@ import React, { createContext, useState, useContext, useEffect } from "react";
 const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-	const [theme, setTheme] = useState("light"); // Initialize theme state to 'light'
+	const [theme, setTheme] = useState(localStorage.getItem("theme") || "light"); // Initialize theme state to 'light'
 
 	// Function to toggle between 'light' and 'dark' themes
 	const toggleTheme = () => {
-		setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+		const newTheme = theme === "light" ? "dark" : "light";
+		setTheme(newTheme);
+		localStorage.setItem("theme", newTheme);
 	};
 
 	// Apply the theme to the document body when the theme state changes
 	useEffect(() => {
+		localStorage.getItem("theme");
 		document.body.setAttribute("data-theme", theme);
 	}, [theme]);
 
